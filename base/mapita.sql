7a9a12efd6de2491354b890a10c9e12192302788
juan121694 <juan121694@gmail.com>
2019-04-05 04:58:12 -0600

    error base corregido

--- base/mapita.sql
+++ base/mapita.sql
@@ -1,45 +1,46 @@
-DROP TABLE IF EXISTS usuario;
+DROP TABLE IF EXISTS comentario;
 
 DROP TABLE IF EXISTS marcador;
 
 DROP TABLE IF EXISTS tema;
 
-DROP TABLE IF EXISTS comentario;
+DROP TABLE IF EXISTS usuario;
 
 CREATE TABLE usuario (
-  idusuario serial NOT NULL,
+  idusuario serial NOT NULL PRIMARY KEY,
   nombre text NOT NULL,
   correo  text NOT NULL,
   contrasenia text NOT NULL,
-  fechanacimiento date,
-  rol text NOT NULL,
-  PRIMARY KEY (idusuario)
+  rol text NOT NULL
 );
 
 CREATE TABLE tema(
 	idtema serial NOT NULL,
-	idusuario integer NOT NULL REFERENCES usuario(idusuario),
+	dueñotema integer NOT NULL REFERENCES usuario(idusuario),
 	nombre text NOT NULL,
 	color text NOT NULL,
-	PRIMARY KEY (idtema,idusuario)
+	PRIMARY KEY (idtema,dueñotema)
 );
 
 CREATE TABLE marcador (
   idmarcador serial NOT NULL,
   idtema integer NOT NULL,
-  idusuario integer NOT NULL,
+  dueñotema integer NOT NULL,
   descripcion text NOT NULL,
   longitud double precision NOT NULL,
   latitud double precision NOT NULL,
-  PRIMARY KEY (idmarcador,idtema,idusuario),
-  FOREIGN KEY (idtema,idusuario) REFERENCES tema(idtema,idusuario) ON DELETE CASCADE
+  FOREIGN KEY (idtema,dueñotema) REFERENCES tema(idtema,dueñotema) ON DELETE CASCADE,
+  PRIMARY KEY (idmarcador,idtema,dueñotema)
+  
 );
 
 CREATE TABLE comentario(
 	idcomentario serial NOT NULL,
 	idmarcador integer NOT NULL,
-	idusuario integer NOT NULL REFERENCES usuario(idusuario) ON DELETE CASCADE,
+    idtema integer NOT NULL,
+    dueñotema integer NOT NULL,
+	comentarista integer NOT NULL REFERENCES usuario(idusuario) ON DELETE CASCADE,
 	comentario text NOT NULL,
-	FOREIGN KEY (idmarcador,idusuario) REFERENCES marcador(idmarcador) ON DELETE CASCADE,
-	PRIMARY KEY (idcomentario,idmarcador,idusuario)
+	FOREIGN KEY (idmarcador,idtema,dueñotema) REFERENCES marcador(idmarcador,idtema,dueñotema) ON DELETE CASCADE,
+	PRIMARY KEY (idcomentario,comentarista,idmarcador)
 );
